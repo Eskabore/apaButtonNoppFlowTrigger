b60782b8f298c26ecc994749d16c92defb6869e0
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2024-02-20 14:31:49 +0100

    Added error messages, dynamic button text display + disable button when loading

--- force-app/main/default/lwc/opportunityFlowTrigger/opportunityFlowTrigger.js
+++ force-app/main/default/lwc/opportunityFlowTrigger/opportunityFlowTrigger.js
@@ -5,29 +5,53 @@ import triggerFlows from '@salesforce/apex/OpportunityFlowTriggerController.trig
 import resetFieldToZero from '@salesforce/apex/ResetFieldToZero.resetStateField';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import STATE_FIELD from '@salesforce/schema/Opportunity.State__c';
+import PRICEBOOK2ID_FIELD from '@salesforce/schema/Opportunity.Pricebook2Id';
 
 export default class OpportunityFlowTrigger extends LightningElement {
     @api recordId;
     opportunityData; // Used to cache the response for refreshApex
+    isLoading = false; // Track loading state
 
-    @wire(getRecord, { recordId: '$recordId', fields: [STATE_FIELD] })
+    @wire(getRecord, { recordId: '$recordId', fields: [STATE_FIELD, PRICEBOOK2ID_FIELD] })
     wiredOpportunity(response) {
         this.opportunityData = response;
         // Error handling is not needed here as we are using getFieldValue below
     }
 
+    get isCorrectPricebook() {
+        const pricebook2Id = getFieldValue(this.opportunityData.data, PRICEBOOK2ID_FIELD);
+        return pricebook2Id === '01s7Q00000JBtVpQAL' || pricebook2Id === null;
+    }
+
+    get buttonLabel() {
+        return this.isLoading ? 'Lädt...' : 'Automatisierte Produkt Auswahl';
+    }
+
     handleClick() {
-        triggerFlows({ opportunityId: this.recordId })
-            .then(() => {
-                this.showToast('Erfolg', 'Einen Moment bitte, die Produkte werden hinzugefügt.', 'success');
-                return refreshApex(this.opportunityData);
-            })
-            .then(() => {
-                setTimeout(() => this.resetState(), 3000); // Delay for 3 seconds
-            })
-            .catch(error => {
-                this.showToast('Fehler', error.body?.message || 'Unbekannter Fehler', 'error');
-            });
+        if (this.opportunityData) {
+            if (this.isCorrectPricebook) {
+                this.isLoading = true;
+                this.showToast('Information', 'Ihre Anfrage wird bearbeitet...', 'info');
+                triggerFlows({ opportunityId: this.recordId })
+                    .then(() => {
+                        this.showToast('Erfolg', 'Einen Moment bitte, die Produkte werden hinzugefügt.', 'success');
+                        return refreshApex(this.opportunityData);
+                    })
+                    .then(() => {
+                        setTimeout(() => this.resetState(), 3000); // Delay for 3 seconds
+                    })
+                    .catch(error => {
+                        this.showToast('Fehler', error.body?.message || 'Unbekannter Fehler', 'error');
+                    })
+                    .finally(() => {
+                        this.isLoading = false;
+                    });
+            } else {
+                this.showToast('Achtung', 'Bitte wählen Sie das Preisbuch "Wärmepumpen Fuchs" vor der automatischen Auswahl.', 'warning');
+            }
+        } else {
+            this.showToast('Fehler', 'Opportunity nicht geladen.', 'error');
+        }
     }
 
     resetState() {
